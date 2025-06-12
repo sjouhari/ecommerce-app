79a9e20389df6a2e90d497d53a45d7b303045c43
Said Jouhari <said.jouhari18@gmail.com>
2025-06-12 21:46:00 +0100

    fix admin and seller guards

--- web_app/src/app/guards/admin-profile.guard.ts
+++ web_app/src/app/guards/admin-profile.guard.ts
@@ -1,9 +1,10 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const adminProfileGuard: CanActivateFn = (route, state) => {
     const authService = inject(AuthService);
+    const router = inject(Router);
 
-    return authService.isAdmin();
+    return authService.isAdmin() ? true : router.createUrlTree(['/home']);
 };
--- web_app/src/app/guards/seller-profile.guard.ts
+++ web_app/src/app/guards/seller-profile.guard.ts
@@ -1,9 +1,10 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const sellerProfileGuard: CanActivateFn = (route, state) => {
     const authService = inject(AuthService);
+    const router = inject(Router);
 
-    return authService.isSeller();
+    return authService.isSeller() ? true : router.createUrlTree(['/home']);
 };
